c88f9b34789960b18bf26bd8d8597279c2a56537
mohAlpiansyahh <pinzthekop@gmail.com>
2024-12-08 14:13:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+let words = document.querySelectorAll(".word");
+words.forEach((word)=>{
+    let letters = word.textContent.split("");
+    word.textContent="";
+    letters.forEach((letter)=>{
+        let span = document.createElement("span");
+        span.textContent = letter;
+        span.className = "letter";
+        word.append(span);
+    });
+});
+
+let currentWordIndex = 0;
+let maxWordIndex = words.length -1;
+words[currentWordIndex].style.opacity = "1";
+
+let changeText = ()=>{
+    let currentWord = words[currentWordIndex];
+    let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+
+    Array.from(currentWord.children).forEach((letter,i)=>{
+        setTimeout(()=>{
+            letter.className = "letter out"; 
+        },i * 80);
+    });
+
+    nextWord.style.opacity="1";
+    Array.from(nextWord.children).forEach((letter,i)=>{
+        letter.className = "letter behind";
+        setTimeout(()=>{
+            letter.className = "letter in";
+        },340 + i * 80);
+    });
+    currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+};
+
+changeText();
+setInterval(changeText,3000)
+
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem=>{
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data=percent");
+    var percent = Math.floor(dots*marked/100);
+    var points = "";
+    var rotate = 360 / dots;
+
+    for(let i = 0 ; i < dots ; i++){
+        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    }
+    elem.innerHTML = points;
+    
+})
+
+var mixer = mixitup('.portofolio-gallery');
+
+
+// sticky menu
+const header = document.querySelector("header");
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",window.scrollY > 50)
+})
+
+
+// toogle
+let menuIcons = document.querySelector("#menu-icons");
+let list = document.querySelector(".list");
+
+menuIcons.onclick = ()=>{
+    menuIcons.classList.toggle("bx-x");
+    list.classList.toggle("open");
+}
+
+window.onscroll = ()=>{
+    menuIcons.classList.remove("bx-x");
+    list.classList.remove("open");
+}
